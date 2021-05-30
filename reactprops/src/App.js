ad11de50d327a18ea86fab1cbd996b5183526ea3
Navin <navinexpo2020@gmail.com>
2021-05-30 23:10:42 +0530

    regular updates

---
+++ reactprops/src/App.js
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Classprops from "./Classprops";
+
+function App() {
+  return (
+    <div><Classprops /></div> 
+  );
+}
+
+export default App;
